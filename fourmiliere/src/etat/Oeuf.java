48be868aa463a273edc509285a42aa5717d1fbff
benoit29200 <contact@autretbenoit.fr>
2017-12-08 15:05:09 +0100

    updatev2.1

--- fourmiliere/src/etat/Oeuf.java
+++ fourmiliere/src/etat/Oeuf.java
@@ -10,15 +10,13 @@ public class Oeuf extends EtatDeveloppement {
 		this.getFourmi().setDureeVie(3); // en jours
 		this.getFourmi().getFourmiliere().getNid().getMesFourmis().add(f);
 	}
-	
+
 	public void cycle() {
-		if(this.getFourmi().getFourmiliere().getNid().getMesFourmis().remove(this.getFourmi())) {
-			this.getFourmi().setEtat(new Larve(this.getFourmi()));
-		}
+		this.getFourmi().setEtat(new Larve(this.getFourmi()));
 	}
-	
+
 	private void setPoids() {
-		Double poids = 0.2 + (double)(Math.random() * ((8.0 - 0.2) + 1.0));
+		Double poids = 0.2 + (double) (Math.random() * ((8.0 - 0.2) + 1.0));
 		this.getFourmi().setPoids(poids);
 	}
 
---
+++ fourmiliere/src/test/test.java
@@ -0,0 +1,14 @@
+package test;
+
+import fourmiliere.Fourmiliere;
+
+public class test {
+
+	public static void main(String[] args) {
+		Fourmiliere f = new Fourmiliere();
+		f.start();
+		System.out.println("ok");
+
+	}
+
+}
